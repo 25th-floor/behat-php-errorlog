2f58626602ff9a441edfe8aecf55929d4b607920
Thomas Subera <ts@25th-floor.com>
2014-11-26 12:45:11 +0100

    first commit
    add first version of the context and some documentation for it

--- README.md
+++ README.md
@@ -1,4 +1,73 @@
 behat-php-errorlog
 ==================
 
-PHP Error Logger Context
+PHP Error Logger Context for Behat 3.
+
+The Context extends from Mink so you need a mink driver running. Basically it creates a log file for every Test Scenario
+in the beforeScenario Hook and after the Scenario it looks into the log file if there is something present.
+
+To get Errors logged within these Error Log Files the application itself needs to be adjusted. This Context sends the
+necessary information to your application but the application itself needs to know what to do with it.
+
+For this the application and the behat runner must be on the same machine, so both can access the same error log file.
+
+
+Selenium
+--------
+
+Selenium does not provide a way to send such meta information via Headers or something else. The easiest way to get
+information to your application therefore is to call an url (which must be provided in the configuration of the context)
+with the necessary information and handle everything else within the application. (for example via cookie).
+
+In our case, the Context will call the provided url with the full path to the error log file.
+
+
+Installation
+============
+
+Just install via composer
+
+
+Configuration
+=============
+
+behat
+-----
+
+Add the context to your behat confguration file. There are three Constructor Parameters:
+
+ * `directory` it needs the Directory where to put the log files.
+
+ * `url` optional but required for selenium test scenarios. See Selenium Part
+
+ * `size` optional, definition after which size it will no longer output the contents of the error log file to your output. Defaults to 2014, use 0 to disable it.
+
+
+your application
+----------------
+
+Depending if it's a normal Scenario or Selenium you need to adjust your application to work with the context.
+
+For Selenium Scenarios you have to provide the url and then save the error_log, for example within a cookie. Other
+Scenarios will get the absolute path to the error log using the request Headser `X-BEHAT-PHP-ERROR-LOG`.
+
+With the absolute path to the error log one must set the php configuration:
+
+```php
+ini_set("log_errors", 1);
+ini_set("error_log", $path);
+```
+
+Scenarios
+---------
+
+You don't need to adjust anything in your Scenarios to get this working. But there is way if you don't want the Context
+to run at a specific Scenario or a whole Feature File. You can use the tag `@ignore-php-logging` if you don't want any
+php action to be logged.
+
+Todo
+====
+
+* Get Wep Api working
+
+* maybe don't depend that behat and application are on the same machine?
---
+++ src/PhpErrorsContext.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace TwentyFifth\Behat\PhpErrorLog;
+
+use Behat\Behat\Hook\Scope\AfterScenarioScope;
+use Behat\Behat\Hook\Scope\BeforeScenarioScope;
+use Behat\Behat\Hook\Scope\ScenarioScope;
+use Behat\MinkExtension\Context\RawMinkContext;
+
+/**
+ * Behat PHP Errors Context
+ *
+ * Setup a php error log file for each scenario and check afterwards if there was something logged
+ *
+ * works with selenium and non selenium, not yet with webapi.
+ *
+ * @author Thomas Subera <ts@25th-floor.com>
+ * @package TwentyFifth\Behat\PhpErrorLog
+ */
+class PHPErrorsContext extends RawMinkContext
+{
+    CONST IGNORE_TAG = 'ignore-php-logging';
+
+    /** @var  string Path to the Log Files */
+    private $path;
+
+    /** @var int maximum output size */
+    private $maxSize = 1024;
+
+    /** @var  string */
+    private $url;
+
+    /** @var  string cache filename for step */
+    private $filename;
+
+    /**
+     * @param string $path Directory where to put the log files
+     * @param string $url Url for Selenium Tests
+     * @param int    $size Maximum FileSize which is outputted to buffer, defaults to 1024
+     */
+    function __construct($path, $url = null, $size = null)
+    {
+        $this->path = $path;
+
+        if ($url !== null) {
+            $this->url = $url;
+        }
+
+        if ($size !== null && $size > 0) {
+            $this->maxSize = $size;
+        }
+    }
+
+    /**
+     * prepare php to log errors to a specific file
+     *
+     * @BeforeScenario
+     *
+     * @param BeforeScenarioScope $scope
+     *
+     * @throws \Exception if file could not be written
+     */
+    public function preparePHPErrorLogFile(BeforeScenarioScope $scope)
+    {
+        $this->filename = false;
+        // no need to do something if this is an empty scenario
+        if (!$scope->getScenario()->hasSteps() && !$scope->getFeature()->hasBackground()) {
+            return;
+        }
+
+        // ignore scenarios with the ignore tag
+        if ($scope->getScenario()->hasTag(self::IGNORE_TAG) || $scope->getFeature()->hasTag(self::IGNORE_TAG)) {
+            return;
+        }
+
+        $path = $this->getFilePath($this->createFileName($scope));
+
+        // cleanup left overs
+        if (file_exists($path)) {
+            unlink($path);
+        }
+
+        // check for write access
+        if (!touch($path)) {
+            throw new \Exception("File $path could not be created!");
+        }
+
+        $this->filename = $path;
+
+        if ($this->getMink()->getDefaultSessionName() == 'selenium2') {
+            if ($this->url == null) {
+                throw new \Exception('Need Url Configuration for selenium tests');
+            }
+            echo "Sending Database Name through " . $this->url . $path . PHP_EOL;
+            $this->visitPath($this->url . urlencode($path));
+            $this->filename = false; // don't do anything stupid
+            return;
+        }
+
+        echo "Set PHP Error Log to " . $path . PHP_EOL;
+
+//        ini_set("log_errors", 1);
+//        ini_set("error_log", $path);
+
+    }
+
+    /**
+     * need for non selenium tests
+     *
+     * @BeforeStep
+     */
+    public function sendHeader()
+    {
+        $filename = $this->filename;
+
+        if (!$filename) {
+            return;
+        }
+
+        $this->getSession()->setRequestHeader('X-BEHAT-PHP-ERROR-LOG', $filename);
+    }
+
+    /**
+     * Checks if php has logged something and if it did, returns the output. But only if the file is not too big.
+     * (Don't want to spam the output window)
+     *
+     * @AfterScenario
+     *
+     * @param AfterScenarioScope $scope
+     *
+     * @throws \Exception
+     */
+    public function lookForPHPErrors(AfterScenarioScope $scope)
+    {
+        // ignore scenarios with the ignore tag
+        if ($scope->getScenario()->hasTag(self::IGNORE_TAG)) {
+            return;
+        }
+
+        $path = $this->getFilePath($this->createFileName($scope));
+
+        // nothing in the file
+        $filesize = filesize($path);
+        if (0 == $filesize) {
+            return;
+        }
+
+        $message = sprintf(
+            'PHP Error Log "%s" is not empty (%d bytes)',
+            $path,
+            $filesize
+        );
+
+        if ($filesize <= $this->maxSize) {
+            echo 'PHP Error Log:' . PHP_EOL;
+
+            readfile($path);
+        }
+
+        throw new \Exception($message);
+    }
+
+    /**
+     * @param ScenarioScope $scope
+     *
+     * @return string
+     */
+    protected function createFileName(ScenarioScope $scope)
+    {
+        $filename = basename($scope->getFeature()->getFile()) . '.' . $scope->getScenario()->getLine();
+        return urlencode(
+            str_replace(' ', '_', strtolower($filename))
+        ) . '.log';
+    }
+
+    /**
+     * return the absolute log file path
+     *
+     * @param string $filename
+     *
+     * @return string
+     */
+    protected function getFilePath($filename)
+    {
+        $path = str_replace('//', '/', $this->path . DIRECTORY_SEPARATOR);
+
+        return $path . $filename;
+    }
+
+}
